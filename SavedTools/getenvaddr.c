457fa49fb7efd1409ce3c21d99b9a76a5f916ba3
Cerkoryn <30681834+Cerkoryn@users.noreply.github.com>
2019-07-25 07:11:50 -0500

    From Hacking: AoE

--- SavedTools/getenvaddr.c
+++ SavedTools/getenvaddr.c
@@ -3,14 +3,13 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  char *ptr;
-  
-  if (argc < 3) {
-    printf("Usage: %s <environment var> <target program name>\n", argv[0]);
-    exit(0);
-    
-  }
-  ptr = getenv(argv[1]); /* Get env var location. */
-  ptr += (strlen(argv[0]) - strlen(argv[2]))*2; /* Adjust for program name. */
-  printf("%s will be at %p\n", argv[1], ptr);
+	char *ptr;
+
+	if(argc < 3) {
+		printf("Usage: %s <environment variable> <target program name>\n", argv[0]);
+		exit(0);
+	}
+	ptr = getenv(argv[1]); /* get env var location */
+	ptr += (strlen(argv[0]) - strlen(argv[2]))*2; /* adjust for program name */
+	printf("%s will be at %p\n", argv[1], ptr);
 }
